bf2015756946d22021720d0f368875130401c45c
Francesco Sullo <sullof@gmail.com>
2020-07-03 18:43:51 -0700

    Renaming

--- src/Server.js
+++ src/Server.js
@@ -23,31 +23,24 @@ class Server {
     })
 
     try {
-      // var options = {
-      //   SNICallback: function (domain, cb) {
-      //
-      //     console.log('domain', domain)
-      //
-      //     let what = domain.split('.').length === 2 ? 'base' : 'wildcard';
-      //     console.log(secureContext[what])
-      //     if (secureContext[what]) {
-      //       if (cb) {
-      //         cb(null, secureContext[what]);
-      //       } else {
-      //         // compatibility for older versions of node
-      //         return secureContext[what];
-      //       }
-      //     } else {
-      //       throw new Error('No keys/certificates for domain requested');
-      //     }
-      //   },
-      //   key: baseKey,
-      //   cert: baseCert
-      // }
-      const server = https.createServer({
+      var options = {
+        SNICallback: function (domain, cb) {
+          let what = domain.split('.').length === 2 ? 'base' : 'wildcard';
+          if (secureContext[what]) {
+            if (cb) {
+              cb(null, secureContext[what]);
+            } else {
+              // compatibility for older versions of node
+              return secureContext[what];
+            }
+          } else {
+            throw new Error('No keys/certificates for domain requested');
+          }
+        },
         key: baseKey,
         cert: baseCert
-      }); //, app.callback);
+      }
+      const server = https.createServer(options, app); //, app.callback);
       //     , function (req, res) {
       //   res.end('Your dynamic SSL server worked!')
       //   // Here you can put proxy server routing here to send the request
@@ -59,7 +52,6 @@ class Server {
       console.error(err.message);
       console.error(err.stack);
     }
-    // Inside the server you can use nodejs
 
   }
 
