4230fa72aa538b7c25bf654e8037eba4750872e1
Francesco Sullo <sullof@gmail.com>
2020-07-03 18:37:15 -0700

    Renaming

--- src/Server.js
+++ src/Server.js
@@ -6,14 +6,12 @@ class Server {
 
   static get(app) {
 
-    let baseCert = fs.readFileSync(process.env.BASE_CERT, 'utf8')
-    let baseKey = fs.readFileSync(process.env.BASE_KEY, 'utf8')
-    let wildcardCert = fs.readFileSync(process.env.WILDCARD_CERT, 'utf8')
-    let wildcardKey = fs.readFileSync(process.env.WILDCARD_KEY, 'utf8')
+    const baseCert = fs.readFileSync(process.env.BASE_CERT, 'utf8')
+    const baseKey = fs.readFileSync(process.env.BASE_KEY, 'utf8')
+    const wildcardCert = fs.readFileSync(process.env.WILDCARD_CERT, 'utf8')
+    const wildcardKey = fs.readFileSync(process.env.WILDCARD_KEY, 'utf8')
 
     const secureContext = {}
-
-    console.log(baseKey, baseCert)
 
     secureContext.base = tls.createSecureContext({
       key: baseKey,
@@ -25,28 +23,31 @@ class Server {
     })
 
     try {
-      var options = {
-        SNICallback: function (domain, cb) {
-
-          console.log('domain', domain)
-
-          let what = domain.split('.').length === 2 ? 'base' : 'wildcard';
-          console.log(secureContext[what])
-          if (secureContext[what]) {
-            if (cb) {
-              cb(null, secureContext[what]);
-            } else {
-              // compatibility for older versions of node
-              return secureContext[what];
-            }
-          } else {
-            throw new Error('No keys/certificates for domain requested');
-          }
-        },
+      // var options = {
+      //   SNICallback: function (domain, cb) {
+      //
+      //     console.log('domain', domain)
+      //
+      //     let what = domain.split('.').length === 2 ? 'base' : 'wildcard';
+      //     console.log(secureContext[what])
+      //     if (secureContext[what]) {
+      //       if (cb) {
+      //         cb(null, secureContext[what]);
+      //       } else {
+      //         // compatibility for older versions of node
+      //         return secureContext[what];
+      //       }
+      //     } else {
+      //       throw new Error('No keys/certificates for domain requested');
+      //     }
+      //   },
+      //   key: baseKey,
+      //   cert: baseCert
+      // }
+      const server = https.createServer({
         key: baseKey,
         cert: baseCert
-      }
-      const server = https.createServer(options); //, app.callback);
+      }); //, app.callback);
       //     , function (req, res) {
       //   res.end('Your dynamic SSL server worked!')
       //   // Here you can put proxy server routing here to send the request
